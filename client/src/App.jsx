9b7db66966faf3d095a0df275c6b893d3f971321
Fathima Shazina <fshazina@icptechno.com>
2024-05-18 18:06:30 +0530

    footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,26 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import SignIn from "./pages/Signin"
-import SignUp from "./pages/SignUp"
-import Dashboard from "./pages/Dashboard"
-import Products from "./pages/Products"
-import Header from "./components/Header"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Products from "./pages/Products";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-export default function App(){
-  return(
-<BrowserRouter>
-<Header/>
-<Routes>
-  <Route path="/" element={<Home/>}/>
-  <Route path="/about" element={<About/>}/>
-  <Route path="/sign-in" element={<SignIn/>}/>
-  <Route path="/sign-up" element={<SignUp/>}/>
-  <Route path="/dashboard" element={<Dashboard/>}/>
-  <Route path="/products" element={<Products/>}/>
-</Routes>
-</BrowserRouter>
-  )
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
