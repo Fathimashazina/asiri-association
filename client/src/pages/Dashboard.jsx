09e515cb53aac4d2c4ce36ad1c5c01146442ff12
Fathima Shazina <fshazina@icptechno.com>
2024-05-22 10:44:41 +0530

    sidebar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,25 @@
-export default function Dashboard(){
-    return(
-        <div>Dashboard</div>
-    )
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* left-side bar */}
+        <DashSidebar />
+      </div>
+      {/* right- */}
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
 }
